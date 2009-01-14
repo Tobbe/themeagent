a5eeabf8365eb27f89722c12309895c0960026d2
Tobbe Lundberg <tobbesweden@gmail.com>
2009-01-14 11:17:03 -0500

    switched to using the basic.rc file instead of the file I used to generate.

--- themeagenttests/rcfiletests.cpp
+++ themeagenttests/rcfiletests.cpp
@@ -8,81 +8,35 @@
 
 using namespace std;
 
-class SetupFixtureRCFile
-{
-public:
-	string rc12;
-
-	SetupFixtureRCFile()
-	{
-		ofstream rcfile;
-		rc12 = "rc12.rc";
-		rcfile.open(rc12.c_str(), ios::out | ios::trunc);
-
-		if (!rcfile.is_open())
-		{
-			assert(false);
-		}
-		else
-		{
-			rcfile << "   \tsettingOne    uno" << endl;
-			rcfile << "\t\t settingTwo    dos" << endl;
-			rcfile << "     settingThree  tres" << endl;
-			rcfile << endl;
-			rcfile << "\t   settingFour   \"red green blue\"  \t  " << endl;
-			rcfile << endl;
-			rcfile << "*test 'MessageBox(NULL, \"message box text\", \"caption\", MB_OK);'" << endl;
-			rcfile << "*test 'cout << \"text to standard output\" << endl;'" << endl;
-			rcfile << "*test 'fstream(\"Twelve: %d, Three and three quaters: %f, Fifty six: %X\", 12, 15/4.0, 56)'" << endl;
-			rcfile << endl;
-			rcfile << "*single 'this is the only \"*single\"'" << endl;
-			rcfile << endl;
-			rcfile << "settingFive   cuatro" << endl;
-			rcfile << "settingSix" << endl;
-			rcfile << "settingTrue true" << endl;
-			rcfile << "settingFalse no" << endl;
-
-			rcfile.close();
-		}
-	}
-
-	~SetupFixtureRCFile()
-	{
-		DeleteFile(rc12.c_str());
-	}
-
-};
-			
-
 SUITE(RCFile)
 {
-	TEST_FIXTURE(SetupFixtureRCFile, Constructor)
+	TEST(Constructor)
 	{
-		RCFile rc(rc12);
-		CHECK(rc.lines() == 12);
+		RCFile rc("TestFiles//RCFiles//basic.rc");
+		CHECK(rc.lines() == 13);
 	}
 
-	TEST_FIXTURE(SetupFixtureRCFile, Get)
+	TEST(Get)
 	{
-		RCFile rc(rc12);
+		RCFile rc("TestFiles//RCFiles//basic.rc");
 
 		CHECK(rc.get("settingone") == "uno");
 		CHECK(rc.get("doesntexist") == "");
 		CHECK(rc.get("settingFour") == "red green blue");
 	}
 
-	TEST_FIXTURE(SetupFixtureRCFile, IsSet)
+	TEST(IsSet)
 	{
-		RCFile rc(rc12);
+		RCFile rc("TestFiles//RCFiles//basic.rc");
 
 		CHECK(rc.isSet("doesntexist") == false);
 		CHECK(rc.isSet("settingone") == true);
 		CHECK(rc.isSet("settingSix") == true);
 	}
 
-	TEST_FIXTURE(SetupFixtureRCFile, GetMultiple)
+	TEST(GetMultiple)
 	{
-		RCFile rc(rc12);
+		RCFile rc("TestFiles//RCFiles//basic.rc");
 
 		CHECK(rc.getMultiple("settingOne") == "uno");
 
@@ -104,9 +58,9 @@ SUITE(RCFile)
 		CHECK(rc.getMultiple("*test") == "MessageBox(NULL, \"message box text\", \"caption\", MB_OK);");
 	}
 
-	TEST_FIXTURE(SetupFixtureRCFile, isTrue)
+	TEST(isTrue)
 	{
-		RCFile rc(rc12);
+		RCFile rc("TestFiles//RCFiles//basic.rc");
 
 		CHECK(rc.isTrue("settingSix"));
 		CHECK(rc.isTrue("settingTrue"));
