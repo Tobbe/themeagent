274dc9fcbd9feb84cc8352d6057ac5dfc14e36ea
Tobbe Lundberg <tobbesweden@gmail.com>
2009-01-26 10:17:45 -0500

    Improved Theme::parseName()

--- themeagent/Theme.cpp
+++ themeagent/Theme.cpp
@@ -16,6 +16,16 @@ string Theme::parseName(RCFile rc) const
 	if (name == "")
 	{
 		//parse name from path
+		size_t index = path.find_last_of("\\/");
+		if (index == path.length() - 1)
+		{
+			index = path.find_last_of("\\/", path.length() - 2);
+			name = path.substr(index + 1, path.length() - 2 - index);
+		}
+		else
+		{
+			name = path.substr(index + 1);
+		}
 	}
 
 	return name;
--- themeagenttests/themetests.cpp
+++ themeagenttests/themetests.cpp
@@ -15,6 +15,15 @@ SUITE(Theme)
 		Theme t("TestFiles\\ThemeTestOne", rc);
 
 		CHECK(t.getName() == "ThemeTestOne");
+
+		RCFile rc2("TestFiles\\Turtle Soup II\\theme.rc");
+		Theme t2("TestFiles\\Turtle Soup II", rc2);
+
+		CHECK(t2.getName() == "Turtle Soup II");
+
+		Theme t3("TestFiles\\Turtle Soup II\\", rc2);
+
+		CHECK(t3.getName() == "Turtle Soup II");
 	}
 
 	TEST(GetAuthor)
