cd27bb1b5b98c573b5fa64a01941052961c428b3
Tobbe Lundberg <tobbesweden@gmail.com>
2009-01-28 13:34:05 -0500

    Added [] operator to ThemeList.

--- themeagent/ThemeList.cpp
+++ themeagent/ThemeList.cpp
@@ -12,3 +12,15 @@ void ThemeList::addTheme(Theme t)
 {
 	themeList.push_back(t);
 }
+
+Theme ThemeList::operator [](size_t index) const
+{
+	if (index >= 0 && index < themeList.size())
+	{
+		return themeList[index];
+	}
+	else
+	{
+		return Theme();
+	}
+}
--- themeagenttests/themelisttests.cpp
+++ themeagenttests/themelisttests.cpp
@@ -17,4 +17,18 @@ SUITE(ThemeList)
 		tl.addTheme(Theme("", rc));
 		CHECK(tl.size() == 3);
 	}
+
+	TEST(IndexingOperator)
+	{
+		ThemeList tl;
+		RCFile rc("");
+		tl.addTheme(Theme("Test1", rc));
+		tl.addTheme(Theme("Test2", rc));
+		tl.addTheme(Theme("Test3", rc));
+		CHECK(tl[0].getName() == "Test1");
+		CHECK(tl[2].getName() == "Test3");
+		CHECK(tl[1].getName() == "Test2");
+
+		CHECK(tl[10].getName() == "");
+	}
 }
