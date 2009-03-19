d241fa42a7fe4a71fc4a9781c27f913028f62268
Tobbe Lundberg <tobbesweden@gmail.com>
2009-03-19 12:23:57 -0400

    Corrected file reading loop check.

--- themeagent/RCFile.cpp
+++ themeagent/RCFile.cpp
@@ -44,9 +44,8 @@ void RCFile::readFile(const string &path)
 
 	if (fin.is_open())
 	{
-		while (!fin.eof() )
+		while (getline(fin, line))
 		{
-			getline(fin, line);
 			line = trim(line);
 			if (line.length() > 0 && line[0] != ';')
 			{
